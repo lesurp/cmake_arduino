653045eaaff2be1ee3d78c1c4f26a49fd17c1592
Paul Lesur <paul.lesur59@gmail.com>
2018-10-26 19:35:32 +0200

    first commit

---
+++ src/main.cpp
@@ -0,0 +1,40 @@
+#include <Arduino.h>
+
+//after build -- to flash
+//avr-objcopy -O ihex -R .eeprom blink.out blink.hex
+//sudo avrdude -V -c dragon_isp -p m168 -b 19200 -P usb -U flash:w:cmaketest.hex
+
+//http://zedcode.blogspot.com/2007/02/gcc-c-link-problems-on-small-embedded.html
+//to keep from needing to link to c++ std lib, maybe? not totally sure
+// solves this error:
+//../../libarduino/libarduinocore.a(Print.o):(.data+0x6): undefined reference to `__cxa_pure_virtual'
+extern "C" void __cxa_pure_virtual(void)
+{
+	// call to a pure virtual function happened ... wow, should never happen ... stop
+	for(;;)
+	{
+
+	}
+}
+
+int main()
+{
+	init();
+
+	Serial.begin(9600);
+	pinMode(13, OUTPUT);
+	for(;;)
+	{
+		Serial.println("Hello");
+		digitalWrite(13, HIGH);
+		delay(2000);
+		digitalWrite(13, LOW);
+		delay(200);
+		digitalWrite(13, HIGH);
+		delay(200);
+		digitalWrite(13, LOW);
+		delay(5000);
+	}
+	return 0;
+}
+
