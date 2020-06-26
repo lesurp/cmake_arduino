2c26fc20a8a38b3fbe374e490e57dd038992f9af
Paul Lesur <git@lesurpaul.fr>
2020-06-26 22:21:45 +0200

    add optimization level switch

--- cmake/avr-toolchain.cmake
+++ cmake/avr-toolchain.cmake
@@ -9,9 +9,8 @@ set(CDEBUG "-gstabs")
 # Many Wstrict-prototypes in arduinocore headers...
 set(CWARN "-Wall -Wextra")
 set(CTUNING "-funsigned-char -funsigned-bitfields -fpack-struct -fshort-enums")
-set(COPT "-O3")
 set(CMCU "-mmcu=atmega328p")
 set(CDEFS "-DF_CPU=16000000")
 
-set(CMAKE_C_FLAGS "${CMCU} ${CDEBUG} ${CDEFS} ${CINCS} ${COPT} ${CWARN} ${CSTANDARD} ${CEXTRA}")
-set(CMAKE_CXX_FLAGS "${CMCU} ${CDEFS} ${CINCS} ${COPT} ${CXXSTANDARD}")
+set(CMAKE_C_FLAGS "${CMCU} ${CDEBUG} ${CDEFS} ${CINCS} ${OPTIMIZATION_LEVEL} ${CWARN} ${CSTANDARD} ${CEXTRA}")
+set(CMAKE_CXX_FLAGS "${CMCU} ${CDEFS} ${CINCS} ${OPTIMIZATION_LEVEL} ${CXXSTANDARD}")
