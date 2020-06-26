bb52af8df75ff0e06d654a0f41ea4b0cbe9a06c8
Paul Lesur <git@lesurpaul.fr>
2020-06-27 01:25:24 +0200

    add visualization

--- tools/print_serial.py
+++ tools/print_serial.py
@@ -3,15 +3,22 @@
 import serial
 import struct
 import sys
+import open3d as o3d
+from scipy.spatial.transform import Rotation as R
 
 ser = serial.Serial(sys.argv[1])
 
 while True:
     d = ser.read()
     if d == b'\n':
+        print("End of packet found, retrieving full packets")
         break
     print("Passing: ", d)
 
+v = o3d.visualization.Visualizer()
+v.create_window()
+mesh = o3d.geometry.TriangleMesh.create_coordinate_frame()
+v.add_geometry(mesh)
 while True:
     data = ser.read(19)
     d = struct.unpack("<ccffffx", data);
@@ -19,3 +26,8 @@ while True:
         print("Malformed packet!")
         continue
     print(d)
+    r = R.from_quat([d[3], d[4], d[5], d[2]])
+    mesh.rotate(r.as_matrix(), center=(0,0,0))
+    v.update_geometry(mesh)
+    v.poll_events()
+    v.update_renderer()
