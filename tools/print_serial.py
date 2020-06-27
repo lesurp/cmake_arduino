8638d085976ab34a948c4490f992faa5a1e97acc
Paul Lesur <git@lesurpaul.fr>
2020-06-27 14:29:01 +0200

    stupid thing does not allow setting R...

--- tools/print_serial.py
+++ tools/print_serial.py
@@ -4,10 +4,9 @@ import serial
 import struct
 import sys
 import open3d as o3d
-from scipy.spatial.transform import Rotation as R
+import numpy
 
 ser = serial.Serial(sys.argv[1])
-
 while True:
     d = ser.read()
     if d == b'\n':
@@ -19,6 +18,7 @@ v = o3d.visualization.Visualizer()
 v.create_window()
 mesh = o3d.geometry.TriangleMesh.create_coordinate_frame()
 v.add_geometry(mesh)
+rprev = o3d.geometry.TriangleMesh.get_rotation_matrix_from_quaternion([1, 0, 0, 0])
 while True:
     data = ser.read(19)
     d = struct.unpack("<ccffffx", data);
@@ -26,8 +26,10 @@ while True:
         print("Malformed packet!")
         continue
     print(d)
-    r = R.from_quat([d[3], d[4], d[5], d[2]])
-    mesh.rotate(r.as_matrix(), center=(0,0,0))
+    r = o3d.geometry.TriangleMesh.get_rotation_matrix_from_quaternion([d[2], d[3], d[4], d[5]])
+    R = numpy.matmul(r, rprev.transpose())
+    mesh.rotate(R, center=(0,0,0))
     v.update_geometry(mesh)
     v.poll_events()
     v.update_renderer()
+    rprev = r
